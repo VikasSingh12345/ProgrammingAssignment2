af1becd845953fee63716f1bf1d69c4bbe9ac471
Vikas Singh <vsingh6_be20@thapar.edu>
2022-09-25 16:09:01 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -11,8 +11,10 @@ makeCacheMatrix <- function(x = matrix()) { ## define the argument with default 
   }
   get <- function() x                     ## define the get fucntion - returns value of the matrix argument
   
-  setinverse <- function(inverse) inv <<- inverse  ## assigns value of inv in parent environment
+  setinverse <- function(sol) inv <<- sol  ## assigns value of inv in parent environment
+  
   getinverse <- function() inv                     ## gets the value of inv where called
+  
   list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  ## you need this in order to refer 
   ## to the functions with the $ operator
 }
@@ -23,11 +25,15 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   inv <- x$getinverse()
   if(!is.null(inv)) {
-    message("getting cached data")
+    message("getting cached DATA")
     return(inv)
   }
+  
   data <- x$get()
+  
   inv <- solve(data, ...)
+  
   x$setinverse(inv)
+  
   inv
 }
